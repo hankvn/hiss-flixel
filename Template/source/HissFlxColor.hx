3f8fb1d980bf7142d98a54947a5b3af2d82a4185
Nat Quayle Nelson <natquaylenelson@gmail.com>
2020-09-15 15:05:27 -0600

    Start wrapping FlxColor

---
+++ Template/source/HissFlxColor.hx
@@ -0,0 +1,8 @@
+import flixel.util.FlxColor;
+
+/**
+    Wraps the abstract type FlxColor from HaxeFlixel as a Hiss-importable class
+**/
+class HissFlxColor {
+
+}
--- Template/source/StartState.hx
+++ Template/source/StartState.hx
@@ -20,20 +20,32 @@ class StartState extends FlxState {
         // TODO put up a loading screen -- make the asset/animation part of the template
 
         var interp = new CCInterp();
+
+        // flixel
         interp.importClass(FlxState, "FlxState");
         interp.importClass(FlxGame, "FlxGame");
         interp.importClass(FlxG, "FlxG");
         interp.importClass(FlxSprite, "FlxSprite");
 
+        // flixel.text
         interp.importClass(FlxText, "FlxText");
 
+        // flixel.ui
         interp.importClass(FlxButton, "FlxButton");
 
+        // flixel.util
+        interp.importClass(HissFlxColor, "FlxColor");
 
         // HissFlixel
         interp.importClass(HissFlxG, "HissFlxG");
         interp.importClass(HissState, "HissState");
 
+        StaticFiles.compileWithAll("lib");
+        for (libFile in StaticFiles.getDirectoryFiles("lib")) {
+            interp.load(libFile);
+        }
+
+        // Game source
         StaticFiles.compileWithAll("hiss");
         interp.load("hiss/main.hiss");
     }
